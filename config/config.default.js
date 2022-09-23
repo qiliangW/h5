a07bb1fac6d803957b3af456b4773b2744c14000
wangqiliang <1039154554@qq.com>
2022-09-23 17:04:58 +0800

    feat:egg指定端口测试

--- config/config.default.js
+++ config/config.default.js
@@ -1,12 +1,12 @@
 /* eslint valid-jsdoc: "off" */
 
-'use strict';
-const path = require('path');
-const fs = require('fs');
+"use strict";
+const path = require("path");
+const fs = require("fs");
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
-module.exports = appInfo => {
+module.exports = (appInfo) => {
   /**
    * built-in config
    * @type {Egg.EggAppConfig}
@@ -14,18 +14,19 @@ module.exports = appInfo => {
   const config = (exports = {});
 
   config.siteFile = {
-    '/': fs.readFileSync(path.join(appInfo.baseDir, 'app/public/dist/index.html')),
-
+    "/": fs.readFileSync(
+      path.join(appInfo.baseDir, "app/public/dist/index.html")
+    ),
   };
   config.static = {
-    prefix: '/',
-    dir: path.join(appInfo.baseDir, 'app/public/dist'),
+    prefix: "/",
+    dir: path.join(appInfo.baseDir, "app/public/dist"),
     preload: false,
     maxAge: 31536000, // in prod env, 0 in other envs
     buffer: true, // in prod env, false in other envs
   };
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1662429774366_2196';
+  config.keys = appInfo.name + "_1662429774366_2196";
 
   // add your middleware config here
   config.middleware = [];
@@ -37,14 +38,21 @@ module.exports = appInfo => {
   // 防护
   config.security = {
     enable: false,
-    domainWhiteList: [ 'http://apitest.dev.com' ],
+    domainWhiteList: ["http://apitest.dev.com"],
+  };
+  config.cluster = {
+    listen: {
+      path: "",
+      port: 9000,
+      hostname: " 0.0.0.0", // 0.0.0.0
+    },
   };
   config.cors = {
     // 任何地址都可以访问
-    origin: '*',
+    origin: "*",
     // 指定地址才可以访问
     // origin: 'http://localhost:8080',
-    allowMethods: 'GET,PUT,POST,DELETE',
+    allowMethods: "GET,PUT,POST,DELETE",
     // cookie跨域配置
     credentials: true,
   };
